c1d9c4068f2d89ce0d75b0793f361923cc1648eb
Baegarden <dnjs0148@naver.com>
2022-05-21 18:16:57 +0900

    get Data

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Nav from '../components/Nav';
 
-export default function Home() {
+export default function Home({ album }) {
+  console.log(album);
   return (
     <div>
       <Nav navLink="/photos" navInfo="Login"></Nav>
@@ -8,3 +9,17 @@ export default function Home() {
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/albums`);
+  const album = await res.json();
+  for (let i = 0; i < album.length; i++) {
+    album[i].color = parseInt(Math.random() * 0xffffff).toString(16);
+  }
+  return {
+    props: {
+      album,
+    },
+    revalidate: 20,
+  };
+};
