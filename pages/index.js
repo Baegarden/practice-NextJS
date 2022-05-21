7646624db96b8ba983e04d85d1305ecd9d10656f
Baegarden <dnjs0148@naver.com>
2022-05-21 18:26:18 +0900

    print data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Nav from '../components/Nav';
 
-export default function Home({ album }) {
-  console.log(album);
+export default function Home() {
   return (
     <div>
       <Nav navLink="/photos" navInfo="Login"></Nav>
@@ -9,17 +8,3 @@ export default function Home({ album }) {
     </div>
   );
 }
-
-export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/albums`);
-  const album = await res.json();
-  for (let i = 0; i < album.length; i++) {
-    album[i].color = parseInt(Math.random() * 0xffffff).toString(16);
-  }
-  return {
-    props: {
-      album,
-    },
-    revalidate: 20,
-  };
-};
--- pages/photos.js
+++ pages/photos.js
@@ -1,12 +1,51 @@
 import Nav from '../components/Nav';
+import photosStyles from '../styles/Photos.module.css';
+import Image from 'next/image';
+import shortid from 'shortid';
 
-const photos = () => {
+const shortKey = require('shortid');
+
+const photos = ({ album }) => {
+  console.log(album);
   return (
     <div>
       <Nav navLink="/" navInfo="Logout"></Nav>
-      <h1>Album Application</h1>
+      <div className={photosStyles.photosContainer}>
+        <h1>Album Application</h1>
+        <ul>
+          {album.map(po => (
+            <li key={shortKey.generate()}>
+              <div>
+                <Image
+                  src={`https://place-hold.it/100/${po.color}?text=Image`}
+                  width={100}
+                  height={100}
+                  className={photosStyles.image}
+                ></Image>
+              </div>
+              <div className={photosStyles.title}>
+                {po.id}. {po.title}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/albums`);
+  const album = await res.json();
+  for (let i = 0; i < album.length; i++) {
+    album[i].color = parseInt(Math.random() * 0xffffff).toString(16);
+  }
+  return {
+    props: {
+      album,
+    },
+    revalidate: 20,
+  };
 };
 
 export default photos;
